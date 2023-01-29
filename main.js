c18dad9e05407d01391a0055e573cf5e52ef9053
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-01-29 09:20:18 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+
+const characters ='abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+const length = prompt("Enter your key: ");
+
+console.log(generateString(length));
